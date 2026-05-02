cdbb843c328526cc44f0e8288b0bfd24a87515b4
luke11brown <13762210+luke11brown@users.noreply.github.com>
2026-05-02 17:55:38 +0400

    iphone PDF scaling

--- js/app.js
+++ js/app.js
@@ -2228,16 +2228,16 @@
     @media print { .report-actions { display:none; } }
     @supports (-webkit-touch-callout: none) {
       @media print {
-        @page { size: A4 landscape; margin: 0; }
         body {
-          margin: 0;
-          padding: 0;
+          overflow: visible;
         }
         .a5-spread {
-          height: 210mm;
-          min-height: 210mm;
-          padding: 6mm;
-          width: 297mm;
+          gap: 6mm;
+          left: 50%;
+          min-height: 226mm;
+          position: relative;
+          transform: translateX(-50%);
+          width: 320mm;
         }
       }
     }
